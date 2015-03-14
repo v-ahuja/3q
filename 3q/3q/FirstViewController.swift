2fb9bb58134cbd54e1fe0041c25e5bde18ebea05
Varun Kohli <kohli_varun5@yahoo.com>
2015-03-13 23:57:49 -0400

    first commit

---
+++ 3q/3q/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  3q
+//
+//  Created by Varun Kohli on 3/13/15.
+//  Copyright (c) 2015 Varun Kohli. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
